56add9e8af4ee4bb27a8829ca8a9682b2df305db
Josh Foster <josh@allenb.com>
2017-02-07 22:53:09 -0600

    first commit

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -23,17 +23,18 @@ class Dashboard extends Component {
           this.setState({
             uid:user.uid
           });
-          uid=user.uid;
-                firebase.database()
-                .ref('/'+uid+'/songs')
-                .on('value',(data)=>{
-                  let snapshot = data.val();
-                  let songs = firebaseListToArray(snapshot);
-                  this.setState({
-                    songs:songs
-                  });
-                  console.log('the Dash CWM songs: ',this.state.songs);
-                });
+          // uid=user.uid;
+          //     firebase.database()
+          //     .ref('/'+uid+'/gigs')
+          //     .on('value',(data)=>{
+          //       let snapshot = data.val();
+          //       console.log('the gig is: ',snapshot);
+          //       let gig = firebaseListToArray(snapshot);
+          //       this.setState({
+          //         gig:gig
+          //       });
+          //       console.log('the Dash CWM gigs: ',this.state.gig);
+          //     });
 
 
         }else{
@@ -45,24 +46,24 @@ class Dashboard extends Component {
 
   }
   componentDidMount(){
-    let time1 = moment.duration('00:6:30');
-    let time2 = moment.duration('00:05:30');
-    let result = time1+time2
-    console.log('milisec: ',time2);
-    result = moment(result).format("m:ss");
-    console.log('time is: ',result);
-    let minutes = 6;
-    let seconds = 30;
-    minutes = moment.duration('00:'+minutes+':00');
-    seconds = moment.duration('00:00:'+seconds);
-    console.log('the minutes are: ',minutes);
-    console.log('the seconds are: ',seconds);
-    let result2 = minutes+seconds;
-    result2 = moment(result2).format("m:ss");
-    console.log('time2 is: ',result2);
-    let testtime = '6:30';
-    testtime = moment(testtime,"m:ss");
-    console.log('testtime *2 = ',testtime+testtime);
+    // let time1 = moment.duration('00:6:30');
+    // let time2 = moment.duration('00:05:30');
+    // let result = time1+time2
+    // console.log('milisec: ',time2);
+    // result = moment(result).format("m:ss");
+    // console.log('time is: ',result);
+    // let minutes = 6;
+    // let seconds = 30;
+    // minutes = moment.duration('00:'+minutes+':00');
+    // seconds = moment.duration('00:00:'+seconds);
+    // console.log('the minutes are: ',minutes);
+    // console.log('the seconds are: ',seconds);
+    // let result2 = minutes+seconds;
+    // result2 = moment(result2).format("m:ss");
+    // console.log('time2 is: ',result2);
+    // let testtime = '6:30';
+    // testtime = moment(testtime,"m:ss");
+    // console.log('testtime *2 = ',testtime+testtime);
 
 
   }
@@ -70,14 +71,15 @@ class Dashboard extends Component {
 
 
   render(){
-    let html = (this.state.songs) ?
+    let mysongs = this.state.gig.sets[0]
+    let html = (mysongs) ?
     <div className="row">
     <SetList />
-    <SongArea songs={this.state.songs} />
+    <SongArea songs={mysongs} />
     </div>
     : '';
-    console.log('dashboard render songs: ',this.state.songs);
-    let songs = this.state.songs;
+    console.log('dashboard render songs: ',mysongs);
+    // let songs = this.state.songs;
     return(
       <div>
       <div className="wrapper container">
