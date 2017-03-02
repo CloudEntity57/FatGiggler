9a5292a91996f6a59995b5c7add1b20d73d47e75
Josh Foster <josh@allenb.com>
2017-03-01 19:35:26 -0600

    gig view now updates title instantly after user edits a song, now i need to branch again and try making gigs load freshly from songs in the database on dashboard view

--- src/components/Gigs.js
+++ src/components/Gigs.js
@@ -216,11 +216,13 @@ class Gigs extends Component{
       this.postGig(gigs,gigid);
     },100);
   }
-  postGig(gig,id){
+  postGig(gigs,id){
     console.log('posting');
-    gig.forEach((val)=>{
+    gigs.forEach((val)=>{
       let gigid = val.id;
       if(gigid===id){
+
+
         // console.log('our gig to preview: ',val.gig);
         let frame = [];
         let setnum = 1;
@@ -232,20 +234,40 @@ class Gigs extends Component{
         let maxsets = parseInt(val.gig.setnum);
         console.log('maxsets: ',maxsets);
         let sets = val.gig.sets;
+        //create an array of the latest songs from the database:
+        let songs = [];
+        let uid = this.state.uid;
+        for(let i=0; i<sets.length; i++){
+          let id = sets[i].id;
+          firebase.database()
+          .ref(uid+'/songs/'+id)
+          .on('value',(data)=>{
+            let result=data.val();
+            result.id=id;
+            songs.push(result);
+          });
+        }
+        console.log('the songs are: ',songs);
 
         //=======================================================
         for(let x=0; x<maxsets; x++){
           let  goods=[];
           // go through every song in the gig:
-          for (var song =0; song<sets.length; song++) {
+          for (var int =0; int<sets.length; int++) {
             // console.log('the song to iterate through: ',sets[song]);
-            if (sets.hasOwnProperty(song)) {
+            if (sets.hasOwnProperty(int)) {
             // check if song has current gig number
-              if(sets[song].set===setnum){
-                console.log('yes its running');
-                //create the ESX for that set
-                // goods.push(<li>{sets[song].title}</li>);
-                goods.push(<div className="gig-item-contain"><li onClick={this.showSong.bind(this)} id={sets[song].id}>{sets[song].title}</li><div className="gig-item">{deleteButton} {editButton}</div></div>);
+            let tune = [];
+              if(sets[int].set===setnum){
+                //grab the matching song from our updated array 'songs'
+                for(let i=0; i<songs.length; i++){
+                  if(songs[i].id===sets[int].id){
+                    tune = songs[i];
+                    console.log('the tune is: ',tune);
+                  }
+                }
+          //create the ESX for that set
+                goods.push(<div className="gig-item-contain"><li onClick={this.showSong.bind(this)} id={tune.id}>{tune.title}</li><div className="gig-item">{deleteButton} {editButton}</div></div>);
 
               }
             }
